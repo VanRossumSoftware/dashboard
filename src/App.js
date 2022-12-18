0cf0504370a6f289e21f3e4f6df3b6815898f783
Brad Van Rossum <bradvrwi@gmail.com>
2022-12-17 18:59:58 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider} from "@mui/material";
+
+//TODO: Left off at timestamp 34:20
+
+function App() {
+  return (
+    <div className="app">
+
+    </div>
+  );
+}
+
+export default App;
